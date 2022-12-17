2c46d1e13f3f597a03251de6c916efb5777a8178
Mekonnen <111089049+Mekonnenageze@users.noreply.github.com>
2022-12-17 12:32:28 +0300

    Update rotl.c

--- rotl.c
+++ rotl.c
@@ -1,26 +1,35 @@
 #include "monty.h"
-/**
-  *f_rotl- rotates the stack to the top
-  *@head: stack head
-  *@counter: line_number
-  *Return: no return
- */
-void f_rotl(stack_t **head,  __attribute__((unused)) unsigned int counter)
-{
-	stack_t *tmp = *head, *aux;
 
-	if (*head == NULL || (*head)->next == NULL)
+/**
+ * get_rotl - function that rotates the tail to the top
+ * @stack: pointer to the top of the stack
+ * @line_number: where the line number appears
+ * Description: 13. rotl
+ * Return: see below
+ * 1. upon success, nothing
+ * 2. upon fail, EXIT_FAILURE
+ */
+void get_rotl(stack_t **stack, unsigned int line_number)
+{
+	stack_t *first, *last;
+
+	(void)line_number;
+
+	if ((*stack == NULL) || ((*stack)->next == NULL))
 	{
-		return;
+		;
 	}
-	aux = (*head)->next;
-	aux->prev = NULL;
-	while (tmp->next != NULL)
+	else
 	{
-		tmp = tmp->next;
+		first = last = *stack;
+		while (last->next)
+		{
+			last = last->next;
+		}
+		*stack = first->next;
+		last->next = first;
+		first->prev = last;
+		first->next = NULL;
+		(*stack)->prev = NULL;
 	}
-	tmp->next = *head;
-	(*head)->next = NULL;
-	(*head)->prev = tmp;
-	(*head) = aux;
 }
